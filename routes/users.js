4ed11b5f669f6b4a1d62874b29055a79bfcdfc2d
anjalimaguluri <magulurinaganjali@gmail.com>
2023-03-29 16:23:02 -0500

    edited user file

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 var express = require('express');
-var router = express.Router();
 var sum=0;
 var more=0;
+var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   more=more+1;
   sum=more+sum;
-  res.send('Sum is :' +sum);
+  res.send('Sum is :'+sum);
 });
 
 module.exports = router;
